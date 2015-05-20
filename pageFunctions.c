e0febdae7e570eddbd22aef0652454d52de7ae77
samdaman619 <samsoon1993@yahoo.com>
2015-05-19 23:28:48 -0700

    Update pageFunctions.c

--- pageFunctions.c
+++ pageFunctions.c
@@ -2,6 +2,12 @@
  Sami Badra
  Assignment 4, CS570
  FILE: pageFunctions.c
+ 
+ Copyright 2015 Sami Badra
+ 
+ DISCLAIMER: Any unauthorized use, including but not limited to, copying or
+ redistributing any chunk of the source code (or an entire file) will result in
+ punishment by law. I, Sami Badra, own all rights to the files and their contents.
  */
 
 #include <stdio.h>
