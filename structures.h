978dfe6d16190a5830c850f4170cefd5650ca676
samdaman619 <samsoon1993@yahoo.com>
2015-05-19 19:14:00 -0700

    structures.h

---
+++ structures.h
@@ -0,0 +1,37 @@
+/*
+ Sami Badra
+ Assignment 4, CS570
+ FILE: structures.h
+ */
+
+#ifndef _pagetable_h
+#define _pagetable_h
+
+typedef enum {false, true} bool;
+
+typedef struct PAGETABLE {
+    struct LEVEL *rootNode;
+    int levelCount;
+    int frameCount;
+    int hits;
+    int misses;
+    unsigned int *bitmaskArray;
+    int *shiftArray;
+    int *entryCount;
+} PAGETABLE;
+
+typedef struct LEVEL {
+    bool isLeafNode;
+    struct PAGETABLE *pageTable;
+    struct LEVEL **nextLevel;
+    struct MAP *map;
+    int depth;
+} LEVEL;
+
+typedef struct MAP {
+    bool isValid;
+    unsigned int frame;
+} MAP;
+
+
+#endif
